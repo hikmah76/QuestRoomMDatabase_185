4ea4522a1d91c60ed604bb6d26562f443451be56
hikmah76 <nur.hikmah190504@gmail.com>
2024-12-17 03:15:36 +0700

    Untuk mengedit data mahasiswa

--- app/src/main/java/com/example/room10/ui/view/mahasiswa/UpdateMhsView.kt
+++ app/src/main/java/com/example/room10/ui/view/mahasiswa/UpdateMhsView.kt
@@ -25,7 +25,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 @Composable
-fun UpdateMhsView(
+fun UpdateMhsView( //Untuk mengedit data mahasiswa
     onBack: () -> Unit,
     onNavigate: () -> Unit,
     modifier: Modifier = Modifier,
